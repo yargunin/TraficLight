565fe1100a1267df4e7cf194bcca865b1080e531
yargunin <vip.yargunin@mail.ru>
2023-02-14 18:59:17 +0400

    Initial Commit

---
+++ TraficLight/ViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  TraficLight
+//
+//  Created by admin on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentLight = CurrentLight.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        redLight.alpha = lightIsOff
+        yellowLight.alpha = lightIsOff
+        greenLight.alpha = lightIsOff
+        
+        startButton.layer.cornerRadius = 10
+        
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        
+    }
+    
+    
+    
+    override func viewWillLayoutSubviews() {}
+    
+    
+    
+    @IBAction func startButtonTab() {
+        if startButton.currentTitle == "Star" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            greenLight.alpha = lightIsOff
+            redLight.alpha = lightIsOn
+            currentLight = .yellow
+            
+        case .yellow:
+            redLight.alpha = lightIsOff
+            yellowLight.alpha = lightIsOn
+            currentLight = .green
+            
+        case .green:
+            yellowLight.alpha = lightIsOff
+            greenLight.alpha = lightIsOn
+            currentLight = .red
+        }
+    }
+}
+
+extension ViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
+}
+
